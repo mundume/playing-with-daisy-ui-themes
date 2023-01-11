be8b330ea65e1163572820540bda672bad8917e6
nzai <nzaih18@gmail.com>
2023-01-11 12:57:26 +0300

    fun

--- src/components/UseRefExample.jsx
+++ src/components/UseRefExample.jsx
@@ -6,19 +6,24 @@ const UseRefExample = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    inputRef.current.className = "bg-yellow-400 p-5 rounded m-1";
+    inputRef.current.className =
+      "bg-yellow-400 p-5 rounded m-1 focus:border-green-500 ";
     paraRef.current.innerHTML = "yaay";
   };
   return (
     <div>
       <form action="" onSubmit={handleSubmit}>
         <label htmlFor="name">Name</label>
-        <input type="text" className="input m-2 bg-base-200 " ref={inputRef} />
+        <input
+          type="text"
+          className="input m-2 bg-base-200 focus:border focus:border-green-400 "
+          ref={inputRef}
+        />
         <button type="submit" className="btn-outline btn ">
           submit
         </button>
       </form>
-      <p ref={paraRef}></p>
+      <p onClick={() => inputRef.current.focus()} ref={paraRef}></p>
     </div>
   );
 };
